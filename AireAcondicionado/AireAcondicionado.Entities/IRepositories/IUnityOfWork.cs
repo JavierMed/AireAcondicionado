c62521a73f45f2789640c15932faf75157303057
JavierMed <josejavier.medina.huaraca@gmail.com>
2017-05-25 00:59:59 -0500

    class UnityOfWork
    create class UnityOfWork for implement with attrib

---
+++ AireAcondicionado/AireAcondicionado.Entities/IRepositories/IUnityOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AireAcondicionado.Entities.IRepositories
+{
+    public interface IUnityOfWork:IDisposable
+    {
+        IAlmacenRepository Almacenes { get; }
+        ICargoRepository Cargos { get; }
+        ICliEmpresaRepository CliEmpresas { get; }
+        IClienteRepository Clientes { get; }
+        ICliNaturalRepository CliNaturales { get; }
+        IComprobantePagoRepository ComprobantePagos { get; }
+        IContactoRepository Contactos { get; }
+        IContratoRepository Contratos { get; }
+        ICotizacionRepository Cotizaciones { get; }
+        IDepartamentoRepository Departamentos { get; }
+        IDistritoRepository Distritos { get; }
+        IDocumentoRepository Documentos { get; }
+        IFormaPagoRepository FormaPagos { get; }
+        IPedidoRepository Pedidos { get; }
+        IPersonaRepository Personas { get; }
+        IProductoRepository Productos { get; }
+        IProveedorRepository Proveedores { get; }
+        IProvinciaRepository Provincias { get; }
+        IServicioRepository Servicios { get; }
+        ITrabajadorRepository Trabajadores { get; }
+        IUbigeoRepository Ubigeos { get; }
+
+        int SaveChange();
+    }
+}
